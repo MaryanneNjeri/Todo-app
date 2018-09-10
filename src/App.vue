1e337b16cf2ce4e9c1ebfa47801d7fd8fb21c843
MaryAnne45 <mary.jereh@gmail.com>
2018-09-10 19:28:05 +0300

    intial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+
+import TodoList from './components/TodoList'
+export default {
+components:{
+TodoList,
+},
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
